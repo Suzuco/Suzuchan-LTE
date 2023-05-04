402b84829789b840be139e84374f2c91d7cd3f88
Suzuco Kitsuneko <whilesharp@gmail.com>
2023-05-04 23:08:53 +0800

    chatgpt: user info prompt + typo fix

--- extensions/chatgpt/chatgpt.py
+++ extensions/chatgpt/chatgpt.py
@@ -5,11 +5,20 @@ from nonebot import on_command, CommandSession
 
 
 @on_command("gpt", aliases=["小铃同学"], only_to_me=False)
-async def fortune(session: CommandSession):
+async def gpt(session: CommandSession):
     ss = session.current_arg_text.strip()
+
+    ev = session.event
+    if ev["message_type"] == "group" and ev["sub_type"] == "normal":
+        user = ev["sender"]["card"] or ev["sender"]["nickname"]
+        source = await session.bot.get_group_info(group_id=ev["group_id"])
+        source = source["group_name"]
+    else:
+        user = ev["sender"]["nickname"]
+        source = ""
 
     res = requests.post(
         url="https://suzuco.moe:13450/api/neko-gpt/",
-        data=json.dumps({"rq": ss}))
+        data=json.dumps({"rq": ss, "by": user, "from": source}))
 
     return await session.send(f"""{res.text}""", at_sender=True)
