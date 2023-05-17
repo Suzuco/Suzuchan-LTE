dd7f6feccecf6d26a42e6e3ac3874bd3312541d1
Suzuco Kitsuneko <whilesharp@gmail.com>
2023-05-17 13:25:25 +0800

    chatgpt: premium alongside standard

--- extensions/chatgpt/chatgpt.py
+++ extensions/chatgpt/chatgpt.py
@@ -4,23 +4,28 @@ import json
 from nonebot import on_command, CommandSession
 
 
-@on_command("gpt", aliases=["小铃同学"], only_to_me=False)
-async def gpt(session: CommandSession):
+def api(session: CommandSession, premium: bool):
     ss = session.current_arg_text.strip()
 
     ev = session.event
     if ev["message_type"] == "group" and ev["sub_type"] == "normal":
         user = ev["sender"]["card"] or ev["sender"]["nickname"]
-        # source = await session.bot.get_group_info(group_id=ev["group_id"])
-        # source = source["group_name"]
-        source = ev["group_id"]
+        source = ev["group_id"] if premium else 0
     else:
         user = ev["sender"]["nickname"]
-        # source = ""
         source = 0
-
-    res = requests.post(
+    return requests.post(
         url="https://suzuco.moe:13450/api/neko-gpt/",
-        data=json.dumps({"rq": ss, "by": user, "from": source}))  # , "from": source}))
+        data=json.dumps({"rq": ss, "by": user, "from": source}))
 
+
+@on_command("gpt", aliases=["小铃同学"], only_to_me=False)
+async def gpt(session: CommandSession, premium=False):
+    res = api(session, False)
+    return await session.send(f"""{res.text}""", at_sender=True)
+
+
+@on_command("grq", only_to_me=False)
+async def gpt_premium(session: CommandSession):
+    res = api(session, True)
     return await session.send(f"""{res.text}""", at_sender=True)
