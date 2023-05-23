601299edcd0701f17fd432586df2884f8aa712d4
Suzuco Kitsuneko <whilesharp@gmail.com>
2023-05-23 10:09:49 +0800

    chatgpt: better application channel support

--- extensions/chatgpt/chatgpt.py
+++ extensions/chatgpt/chatgpt.py
@@ -4,13 +4,13 @@ import json
 from nonebot import on_command, CommandSession
 
 
-def api(session: CommandSession, premium: bool):
+def api(session: CommandSession, channel: str):
     ss = session.current_arg_text.strip()
 
     ev = session.event
     if ev["message_type"] == "group" and ev["sub_type"] == "normal":
         user = ev["sender"]["card"] or ev["sender"]["nickname"]
-        source = ev["group_id"] if premium else 0
+        source = ev["group_id"] if channel == "premium" else 0
     else:
         user = ev["sender"]["nickname"]
         source = 0
@@ -21,11 +21,11 @@ def api(session: CommandSession, premium: bool):
 
 @on_command("gpt", aliases=["小铃同学"], only_to_me=False)
 async def gpt(session: CommandSession, premium=False):
-    res = api(session, False)
+    res = api(session, "")
     return await session.send(f"""{res.text}""", at_sender=True)
 
 
-@on_command("grq", only_to_me=False)
+@on_command("gpt+", aliases=["grq", "grp"], only_to_me=False)
 async def gpt_premium(session: CommandSession):
-    res = api(session, True)
+    res = api(session, "premium")
     return await session.send(f"""{res.text}""", at_sender=True)
